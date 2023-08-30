c415d6e862d7677ab56343c4bc8e81f33fc9186b
sief <sief.eldeen@revca.tech>
2023-08-30 19:10:05 +0300

    message

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,8 @@
-import { defineConfig } from 'vite';
-// Import the Vite React plugin if needed:
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Define your Vite configuration
+// https://vitejs.dev/config/
 export default defineConfig({
-  // Enable plugins if necessary
   // plugins: [react()],
-  "routes": [{ "src": "/[^.]+", "dest": "/", "status": 200 }],
-  // Define custom routing rules
-  // routes: [
-  //   {
-  //     src: '/[^.]+', // Match URLs that don't have a file extension
-  //     dest: '/',     // Redirect to the root path
-  //     status: 200    // Use a 200 status code for the redirection
-  //   }
-  // ],
-
-  // Customize the Vite build configuration
-  // build: {
-  //   rollupOptions: {
-  //     // ...other options
-
-  //     // Mark 'module-name' as an external dependency
-  //     external: ['@firebase/app']
-  //   }
-  // }
-});
-
+  "routes": [{ "src": "/[^.]+", "dest": "/", "status": 200 }] 
+})
