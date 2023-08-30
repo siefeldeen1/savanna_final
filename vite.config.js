b35301e226aac7c1bc7daf95ad732e0e24bd4364
sief <sief.eldeen@revca.tech>
2023-08-30 19:18:35 +0300

    message

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(
   // plugins: [react()],
-  "routes": [{ "src": "/[^.]+", "dest": "/", "status": 200 }] 
+  {
+    "rewrites":  [
+      {"source": "/(.*)", "destination": "/"}
+    ]
+  
+  
 })
