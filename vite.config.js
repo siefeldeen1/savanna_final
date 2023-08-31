5a84b6a23f9c8ed03430d1256c90bde86fa2c52a
sief <sief.eldeen@revca.tech>
2023-08-31 20:48:25 +0300

    fixes 2

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(
-  // { "routes": [{ "src": "/[^.]+", "dest": "/", "status": 200 }] 
-  
-  {
-    "rewrites":  [
-      {"source": "/(.*)", "destination": "/"}
-    ]
+  { "routes": [{ "src": "/[^.]+", "dest": "/", "status": 200 }] 
   
   
 })
